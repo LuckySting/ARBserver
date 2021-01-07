a6d29959979ac5b73cff9dc4b096fd61fa19cfe3
luckysting <work.romanuel@gmail.com>
2021-01-07 23:24:23 +0700

    Make UVLoop more abstract

--- app/app.py
+++ app/app.py
@@ -1,15 +1,8 @@
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
-from starlette.routing import Route
-
-from app.lib.server import UVLoopServer
+from app.lib.server import HelloServer
 from app.lib.db import Database
 
 
-async def homepage(request):
-    return JSONResponse({'hello': 'world'})
-
-
 def create_app() -> Starlette:
-    app = UVLoopServer.create_server([Route('/', homepage)], [Database.init_db], [Database.shutdown_db])
+    app = HelloServer.create_server([Database.init_db], [Database.shutdown_db])
     return app
--- app/lib/server.py
+++ app/lib/server.py
@@ -2,13 +2,16 @@ import uvloop
 import abc
 from typing import List, Callable
 from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import Response, JSONResponse
 from starlette.routing import Route
 
 
 class IServer(abc.ABC):
     @classmethod
     @abc.abstractmethod
-    def create_server(cls, routes: List[Route], on_startup: List[Callable], on_shutdown: List[Callable]) -> Starlette:
+    def create_server(cls, on_startup: List[Callable], on_shutdown: List[Callable],
+                      routes: List[Route] = None) -> Starlette:
         """
         Server fabric method
         :param routes: routes list for starlette app
@@ -22,6 +25,18 @@ class IServer(abc.ABC):
 class UVLoopServer(IServer):
 
     @classmethod
-    def create_server(cls, routes: List[Route], on_startup: List[Callable], on_shutdown: List[Callable]) -> Starlette:
+    def create_server(cls, on_startup: List[Callable], on_shutdown: List[Callable], routes=None) -> Starlette:
+        if routes is None:
+            routes = []
         uvloop.install()
         return Starlette(routes=routes, on_startup=on_startup, on_shutdown=on_shutdown)
+
+
+class HelloServer(UVLoopServer):
+
+    @classmethod
+    def create_server(cls, on_startup: List[Callable], on_shutdown: List[Callable], routes=None) -> Starlette:
+        async def hello(request: Request) -> Response:
+            return JSONResponse({'hello': 'world'})
+
+        return super().create_server(on_startup, on_shutdown, [Route('/', hello)])
