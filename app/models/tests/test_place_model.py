e1ac3bc75a8284a0b6c60ae36c6c53c650996f69
luckysting <work.romanuel@gmail.com>
2021-01-08 14:19:40 +0700

    Fix tests

---
+++ app/models/tests/test_place_model.py
@@ -0,0 +1,38 @@
+from typing import Awaitable
+
+import asynctest
+from tortoise.contrib.test import finalizer, initializer
+from tortoise.fields import ReverseRelation
+
+from app.models import *
+from app.models.BaseModel import BaseModel
+
+
+class PlaceModelTestCase(asynctest.TestCase):
+    def setUp(self) -> None:
+        initializer(['app.models'])
+
+    def tearDown(self) -> None:
+        finalizer()
+
+    async def test_can_instance_model(self):
+        rest = PlaceModel()
+        self.assertIsInstance(rest, BaseModel)
+
+    async def test_model_table_name(self):
+        rest = PlaceModel()
+        self.assertEqual(rest.Meta.table, 'place')
+
+    async def test_model_has_req_fields(self):
+        place = PlaceModel()
+        self.assertIsNone(place.id)
+        self.assertIsNone(place.created_at)
+        self.assertIsNone(place.updated_at)
+        self.assertIsNone(place.address)
+        self.assertIsNone(place.longitude)
+        self.assertIsNone(place.latitude)
+        self.assertIsInstance(place.gallery, ReverseRelation)
+        self.assertIsNone(place.work_time)
+        self.assertEqual(place.preorder, False)
+        self.assertIsInstance(place.tables, ReverseRelation)
+        self.assertIsInstance(place.restaurant, Awaitable)
--- app/models/tests/test_restaurant_model.py
+++ app/models/tests/test_restaurant_model.py
@@ -1,8 +1,10 @@
+from typing import Awaitable
+
 import asynctest
 from tortoise.contrib.test import finalizer, initializer
 from tortoise.fields import ReverseRelation
 
-from app.models import *
+from app.models import RestaurantModel, FileModel
 from app.models.BaseModel import BaseModel
 
 
@@ -29,14 +31,14 @@ class RestaurantModelTestCase(asynctest.TestCase):
         self.assertIsNone(rest.name)
         self.assertEqual(rest.description, '')
         self.assertEqual(rest.confirmed, False)
-        self.assertIsNone(rest.image)
         self.assertIsInstance(rest.menu, ReverseRelation)
         self.assertIsInstance(rest.places, ReverseRelation)
+        self.assertIsInstance(rest.image, Awaitable)
 
     async def test_can_create_restaurant(self):
-        rest = RestaurantModel()
-        rest.name = 'Test'
-        rest.image = 'test_url'
+        image = FileModel(path='t')
+        await image.save()
+        rest = RestaurantModel(name='Test', image=image)
         await rest.save()
         self.assertEqual(rest.id, 1)
         self.assertIsNotNone(rest.created_at)
@@ -44,21 +46,27 @@ class RestaurantModelTestCase(asynctest.TestCase):
         self.assertEqual(rest.description, '')
         self.assertEqual(rest.confirmed, False)
         self.assertEqual(rest.name, 'Test')
-        self.assertEqual(rest.image, 'test_url')
+        self.assertEqual(rest.image, image)
         self.assertIsInstance(rest.menu, ReverseRelation)
         self.assertIsInstance(rest.places, ReverseRelation)
 
     async def test_can_get_restaurant_list(self):
-        rest = RestaurantModel(name='t1', image='t1')
-        rest2 = RestaurantModel(name='t2', image='t2')
+        image = FileModel(path='t1')
+        image2 = FileModel(path='t2')
+        await image.save()
+        await image2.save()
+        rest = RestaurantModel(name='t1', image=image)
+        rest2 = RestaurantModel(name='t2', image=image2)
         await rest.save()
         await rest2.save()
         rests = await RestaurantModel.all()
-        rests_objs = [(r.id, r.name, r.image) for r in rests]
-        self.assertListEqual(rests_objs, [(1, 't1', 't1'), (2, 't2', 't2')])
+        rests_objs = [(r.id, r.name, await r.image.first()) for r in rests]
+        self.assertListEqual(rests_objs, [(1, 't1', image), (2, 't2', image2)])
 
     async def test_can_get_restaurant_by_id(self):
-        rest = RestaurantModel(name='t1', image='t1')
+        image = FileModel(path='t1')
+        await image.save()
+        rest = RestaurantModel(name='t1', image=image)
         await rest.save()
         rest_obj = await RestaurantModel.get(id=1)
         self.assertEqual(rest, rest_obj)
